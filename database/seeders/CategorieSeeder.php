5e124ed0b5990b613412ac1407d5b5cdbae560f9
nicolashanon <nicolashanon@leschassagnes.net>
2023-01-03 23:38:12 +0100

    Méthode run() : creation 10 categorie

---
+++ database/seeders/CategorieSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\Models\Categorie::factory(10)->create();
+    }
+}

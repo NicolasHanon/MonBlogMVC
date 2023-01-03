e631f948395f92cf9801fbc2889599ee2c0d67b1
nicolashanon <nicolashanon@leschassagnes.net>
2023-01-03 23:49:33 +0100

    Affichage des catégories pour chaque billet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layout')
 @section('contenu')
     @foreach($billets as $billet)
@@ -6,6 +5,20 @@
             <header>
                 <h1 class="titreBillet"><a href="{{ route('billets.show',$billet->id) }}">{{ $billet->BIL_TITRE }}</a>
                 <time>{{ $billet->BIL_DATE }}</time>
+                <p> dans : 
+                    @php
+                        $counter = 0;
+                    @endphp
+                    @foreach($billet->categories as $categorie)
+                        {{ $categorie->CAT_NOM }}
+                        @php
+                            $counter++
+                        @endphp
+                        @if(count($billet->categories) > $counter)
+                            /
+                        @endif
+                    @endforeach
+                </p>
             </header>
             <p> {{ $billet->BIL_CONTENU }}</p>
         </article>

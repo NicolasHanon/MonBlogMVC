75f35e4e893547bb5cbea124ba1b9eb2004dcdd2
nicolashanon <nicolashanon@leschassagnes.net>
2023-01-03 20:10:43 +0100

    Création du modele, du controleur de ressources, de la migration et du factory (billet)

---
+++ app/Models/Billet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Billet extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'BIL_DATE',
+        'BIL_TITRE',
+        'BIL_CONTENU',
+    ];
+}

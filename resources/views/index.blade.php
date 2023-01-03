af54de3a8416fb6c859bd80a9b167f6b91de2c4f
nicolashanon <nicolashanon@leschassagnes.net>
2023-01-03 22:56:30 +0100

    intégration du template à la vue

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layout')
+@section('contenu')
+    @foreach($billets as $billet)
+        <article>  
+            <header>
+                <h1 class="titreBillet"><a href="{{ route('billets.show',$billet->id }}">{{ $billet->BIL_TITRE }}</a>
+                <time>{{ $billet->BIL_DATE }}</time>
+            </header>
+            <p> {{ $billet->BIL_CONTENU }}</p>
+        </article>
+        <hr />
+    @endforeach
+@endsection

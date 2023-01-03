0b3e786a96356f8cb74e2e99cad81c2cf3ad8b31
nicolashanon <nicolashanon@leschassagnes.net>
2023-01-03 22:50:47 +0100

    création du template

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <!--<link rel="stylesheet" href="..\resources\css\style.css"/>-->
+        <link rel="stylesheet" href="{{ URL::asset('style.css') }}" />
+        <title>Mon Blog</title>
+    </head>
+    <body>
+        <div id="global">
+            <header>
+                <a href="{{ route('billets.index') }}"><h1 id="titreBlog">Mon Blog</h1></a>
+                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
+            </header>
+            <div id="contenu">
+                @yield('contenu')
+            </div> <!-- #contenu -->
+            <footer id="piedBlog">
+                Blog réalisé avec PHP, HTML5 et CSS.
+            </footer>
+        </div> <!-- #global -->
+    </body>
+</html>
